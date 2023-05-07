444e4edcadded87d9ec0a9cb987207cf9cdc27eb
Ayushgarg0925 <ayushgarg0925@gmail.com>
2023-05-07 12:56:56 +0530

    appointment page design

--- Health/forms.py
+++ Health/forms.py
@@ -1,7 +1,19 @@
 from django import forms
 from Health.models import Patient
+from django.forms.widgets import NumberInput
 
 class PatientForm(forms.ModelForm):
     class Meta:
         model=Patient
         fields="__all__"
+
+
+    def __init__(self, *args, **kwargs):
+        super(PatientForm, self).__init__(*args, **kwargs)
+        
+
+        
+        self.fields['pdate'].widget.attrs['placeholder'] = 'YYYY-MM-DD'
+        for c in self.fields.values():
+            c.widget.attrs['class'] = 'form-control'
+        
